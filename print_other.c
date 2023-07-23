d46815bb34b600490e40c9208b44c681028ffeeb
Koffi <noumonvikoffidodji@hotmail.com>
2023-07-23 19:19:33 +0000

    simplified function print_other

--- print_other.c
+++ print_other.c
@@ -3,24 +3,17 @@
 
 /**
  * print_other - prints % for %%, nothing for "% " and "%\0"
+ * @pt: va_list from the _printf function to iterate to next argument
  * @c: character following %
  * @curr_n_by: number of bytes printed before now
  * Return: the updated number of bytes printed
  */
 int print_other(va_list pt, char c, int curr_n_by)
 {
-	unsigned int n;
-
 	if (c == ' ' || c == '\0')
-	{
 		curr_n_by = -1;
-	}
-	/*Add more cases here*/
 	else if (c == 'b')
-	{
-		n = (unsigned int)va_arg(pt, int);
-		curr_n_by += print_binary(n);
-	}
+		curr_n_by += print_binary((unsigned int)va_arg(pt, int));
 	else
 	{
 		_putchar('%');
